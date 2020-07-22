82791e82f74ae1dad1bfacd6f17ee93fc1bf347a
t-sakurai816 <poppun_0816@icloud.com>
2020-07-22 15:16:14 +0900

    メアド認証

--- js/info/submit_mail.js
+++ js/info/submit_mail.js
@@ -1,13 +1,13 @@
 // Amazon Cognito 認証情報プロバイダーを初期化します
 AWS.config.region = 'ap-northeast-1'; // リージョン
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: 'ap-northeast-1:5e2c8030-2a39-4d25-a980-cc163cbf617f',
+  IdentityPoolId: 'ap-northeast-1:XXXXXXXXXXXXXXXXXXXXXｘ',
 });
 
 // Amazon Cognito Userpoolの指定＋クライアントアプリの指定
 const poolData = {
-  UserPoolId: 'ap-northeast-1_XHxS2fxC1', //ユーザープールのID
-  ClientId: '12j4en8cbf95ofe2f05nl6lduj' //クライアントアプリの設定上のID
+  UserPoolId: 'ap-northeast-1_XXXXXXXXXX', //ユーザープールのID
+  ClientId: 'XXXXXXXXXXXXXXXXXXXXXXXXXx' //クライアントアプリの設定上のID
 };
 //ユーザープール＋クライアントアプリの情報を格納
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
