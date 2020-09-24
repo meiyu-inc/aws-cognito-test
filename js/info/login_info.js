e97c62f21d53a86e5bb8b76daaebcb521013d660
rhenium <rhe@nium.dev>
2020-09-24 10:05:05 +0900

    fix code

--- js/info/login_info.js
+++ js/info/login_info.js
@@ -1,11 +1,11 @@
-// 1Amazon Cognito 認証情報プロバイダーを初期化します
-// Amazon Cognito 認証情報プロバイダーを初期化します
-AWS.config.region = 'ap-northeast-1'; // リージョン
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: 'ap-northeast-1_hqIuSoIWq',
-});
-//よくわからないけど初期化します
-AWSCognito.config.region = 'ap-northeast-1'; // リージョン(デフォルトだとこのまま)
-AWSCognito.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: 'ap-northeast-1_hqIuSoIWq', // ID プールのID
-});
+//// 1Amazon Cognito 認証情報プロバイダーを初期化します
+//// Amazon Cognito 認証情報プロバイダーを初期化します
+//AWS.config.region = 'ap-northeast-1'; // リージョン
+//AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//  IdentityPoolId: 'ap-northeast-1_hqIuSoIWq',
+//});
+////よくわからないけど初期化します
+//AWSCognito.config.region = 'ap-northeast-1'; // リージョン(デフォルトだとこのまま)
+//AWSCognito.config.credentials = new AWS.CognitoIdentityCredentials({
+//  IdentityPoolId: 'ap-northeast-1_hqIuSoIWq', // ID プールのID
+//});
